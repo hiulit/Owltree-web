a6642f4cf7c26ed4970d53fd11b0ab069ca834f3
hiulit <hiulit@gmail.com>
2017-07-14 19:20:29 +0200

    Added target _blank to Instagram images

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,7 @@ var userFeed = new Instafeed({
   get: 'user',
   userId: '5555136314',
   accessToken: '5555136314.ac3bfd8.0563de251fb54bf0af91387669045341',
-  limit: 6
+  limit: 6,
+  template: '<a href="{{link}}" target="_blank"><img src="{{image}}" /></a>'
 });
 userFeed.run();
