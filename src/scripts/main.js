8e1f2c810070b80fc333b27ac9de8eae503d46bd
hiulit <hiulit@gmail.com>
2017-07-14 18:03:07 +0200

    Baking products

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 var userFeed = new Instafeed({
   get: 'user',
   userId: '5555136314',
-  accessToken: '5555136314.ac3bfd8.0563de251fb54bf0af91387669045341'
+  accessToken: '5555136314.ac3bfd8.0563de251fb54bf0af91387669045341',
+  limit: 6
 });
 userFeed.run();
