f9e276709a3a778c104284b0921b69eb58efd251
hiulit <hiulit@gmail.com>
2017-08-01 10:19:52 +0200

    Added javascript mail validation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,3 +11,48 @@ var scroll = new SmoothScroll('a[href*="#"]', {
   header: '[data-scroll-header]',
   offset: 30
 });
+
+$('#contact-form').on("submit", function(e) {
+  _self = $(this);
+  e.preventDefault();
+
+  $('#contact-form-response').removeClass('alert--success');
+  $('#contact-form-response').html('');
+  _self.find($('input, textarea')).removeClass('danger');
+
+  $.ajax({
+    url: "process.php",
+    data: _self.serialize(),
+    type: "POST",
+    success:function(data){
+      $('#contact-form-response').show();
+      if (data.type === "danger") {
+        $('#contact-form-response').addClass('alert--' + data.type);
+        var ul = $('<ul>');
+        $.each(data.message, function(index, value) {
+          $('#' + value.field).addClass(data.type);
+          ul.append('<li>' + value.text + '</li>');
+          $('#contact-form-response').append(ul);
+        });
+        _self.find($('input, textarea')).keyup(function() {
+          $(this).removeClass(data.type);
+        });
+      } else if (data.type === "success") {
+        $('#contact-form-response').removeClass('alert--danger');
+        $('#contact-form-response').addClass('alert--' + data.type);
+        $('#contact-form-response').html(data.message);
+        _self[0].reset();
+        setTimeout(function() {
+          $('#contact-form-response').hide();
+        }, 3000);
+      }
+    },
+    error: function (e){
+      console.log(e.status + ' ' + e.statusText);
+      $('#contact-form-response').addClass('alert--danger');
+      _self.find($('input:not([type="submit"]), textarea')).addClass('danger');
+      $('#contact-form-response').show();
+      $('#contact-form-response').html('Whoops! Something went wrong! But it\'s on us... We\'re sorry :(<br>Please, try again later.');
+    }
+  });
+});
