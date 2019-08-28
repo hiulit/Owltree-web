d92f26db95a14114b8b197949b1aefef9b354697
hiulit <hiulit@gmail.com>
2019-08-28 17:27:48 +0200

    Removed contact form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -149,7 +149,7 @@ var userFeed = new Instafeed({
   get: 'user',
   userId: '5555136314',
   accessToken: '5555136314.ac3bfd8.0563de251fb54bf0af91387669045341',
-  limit: 6,
+  limit: 12,
   template: '<a href="{{link}}" target="_blank"><img src="{{image}}" /></a>'
 })
 userFeed.run()
